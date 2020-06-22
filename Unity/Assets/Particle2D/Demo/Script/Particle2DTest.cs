af7b15b9d9ec3110a62219100b36fe4a3739ae13
tiankang <495536461@qq.com>
2020-06-22 21:45:56 +0800

    测试提交

---
+++ Unity/Assets/Particle2D/Demo/Script/Particle2DTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle2DTest : MonoBehaviour {
+
+	private Particle2DSystem ps;
+
+	void Awake(){
+		ps = GetComponent<Particle2DSystem>();
+		ps.playOnAwake = false;
+	}
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(2f);
+		print("particle is play");
+		ps.Play();
+		yield return new WaitForSeconds(5f);
+		print("particle is pause");
+		ps.Stop();
+
+
+		yield return new WaitForSeconds(2f);
+		print("particle is play");
+		ps.Play();
+		yield return new WaitForSeconds(5f);
+		print("particle is stop");
+		ps.Stop(true);
+	}
+
+}
